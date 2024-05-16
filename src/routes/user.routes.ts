bfcf72178d48e5b9bf6cd0b16ee375cf22409c17
Fernando <fer3443@gmail.com>
2024-05-16 12:39:37 -0300

    [feature] authenticate agregada a updateUser

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { AddUser, LoginUser, UpdateUser } from "../controllers/user.controller";
+import { authenticateToken } from "../helpers/token.helper";
 
 const router = express.Router();
 router.use(express.json());
 
 router.post("/users/add", AddUser);
 router.post("/users/login", LoginUser);
-router.put('/users/update/:id', UpdateUser)
+router.put("/users/update/:id", authenticateToken, UpdateUser);
 
 export default router;
