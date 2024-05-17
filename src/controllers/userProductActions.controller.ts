fdb96a19fca0f8fa4c8efc1af28e8f6db0bb4aee
Fernando <fer3443@gmail.com>
2024-05-17 16:53:06 -0300

    [create] GetFavorites y route agregadods

--- src/controllers/userProductActions.controller.ts
+++ src/controllers/userProductActions.controller.ts
@@ -1,63 +1,111 @@
 import { Request, Response } from "express";
 import userSchema from "../models/user.models";
 import productsSchema from "../models/products.model";
+//functions to favorites management
+export const AddFavorites = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const { productId } = req.body;
+    if (id === undefined || id === null) {
+      res.status(406).json({ ok: false, msg: "id params invalid" });
+      return;
+    }
+    if (productId === undefined || productId === null) {
+      res.status(406).json({ ok: false, msg: "productId invalid" });
+      return;
+    }
+    const user = await userSchema.findById(id);
+    const favorite = await productsSchema.findById(productId);
+    if (!user || !favorite) {
+      res
+        .status(404)
+        .json({ ok: false, msg: "usuario o producto no encontrado" });
+      return;
+    }
+    if (user.wishList.includes(productId)) {
+      res
+        .status(409)
+        .json({ ok: false, msg: "el producto ya se encuentra en la lista" });
+      return;
+    }
+    user.wishList.push(productId);
+    user.save();
+  } catch (error) {
+    console.error(`Error al agregar favorito ${error}`);
+    res
+      .status(500)
+      .json({ ok: false, msg: `Error al agregar favorito: ${error}` });
+  }
+};
 
-export const AddFavorites = async (req: Request, res: Response): Promise<void> => {
-	try {
-		const {id} = req.params;
-		const {productId} = req.body;
-		if(id === undefined || id === null){
-			res.status(406).json({ok: false, msg: "id params invalid"})
-			return
-		}
-		if(productId === undefined || productId === null){
-			res.status(406).json({ok: false, msg: "productId invalid"})
-			return
-		}
-		const user = await userSchema.findById(id);
-		const favorite = await productsSchema.findById(productId);
-		if(!user || !favorite){
-			res.status(404).json({ok:false, msg: "usuario o producto no encontrado"})
-			return
-		};
-		if(user.wishList.includes(productId)){
-			res.status(409).json({ok: false, msg: "el producto ya se encuentra en la lista"})
-			return
-		}
-		user.wishList.push(productId);
-		user.save();
-	} catch (error) {
-		console.error(`Error al agregar favorito ${error}`);
-		res.status(500).json({ok: false, msg: `Error al agregar favorito: ${error}`})
-	}
-}
+export const DeleteFavorites = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const { productId } = req.body;
+    if (id === undefined || id === null) {
+      res.status(406).json({ ok: false, msg: "id params invalid" });
+      return;
+    }
+    if (productId === undefined || productId === null) {
+      res.status(406).json({ ok: false, msg: "productId invalid" });
+      return;
+    }
+    const user = await userSchema.findById(id);
+    if (!user) {
+      res.status(404).json({ ok: false, msg: "usuario no encontrado" });
+      return;
+    }
+    if (!user?.wishList.includes(productId)) {
+      res
+        .status(404)
+        .json({ ok: false, msg: "producto no encontrado en su wishList" });
+      return;
+    }
+    user.wishList = user.wishList.filter(
+      (favorite) => favorite.toString() !== productId
+    );
+    await user.save();
+    res.status(200).json({ ok: true, msg: "favorito eliminado con éxito" });
+  } catch (error) {
+    console.error(`Error al eliminar favorito ${error}`);
+    res
+      .status(500)
+      .json({ ok: false, msg: `Error al eliminar favorito ${error}` });
+  }
+};
 
-export const DeleteFavorites = async (req: Request, res: Response):Promise<void> => {
-	try {
-		const {id} = req.params;
-		const {productId} = req.body;
-		if(id === undefined || id === null){
-			res.status(406).json({ok: false, msg: "id params invalid"})
-			return
-		}
-		if(productId === undefined || productId === null){
-			res.status(406).json({ok: false, msg: "productId invalid"})
-			return
-		}
-		const user = await userSchema.findById(id);
-		if(!user){
-			res.status(404).json({ok: false, msg: "usuario no encontrado"});
-			return
-		}
-		if(!user?.wishList.includes(productId)){
-			res.status(404).json({ok: false, msg: "producto no encontrado en su wishList"});
-			return
-		}
-		user.wishList = user.wishList.filter((favorite) => favorite.toString() !== productId);
-		await user.save();
-		res.status(200).json({ok:true, msg: "favorito eliminado con éxito"});
-	} catch (error) {
-		console.error(`Error al eliminar favorito ${error}`);
-		res.status(500).json({ok: false, msg: `Error al eliminar favorito ${error}`})
-	}
-}
+export const GetFavorites = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    if (id === undefined || id === null) {
+      res.status(406).json({ ok: false, msg: "id params invalid" });
+      return;
+    }
+    const user = await userSchema.findById(id).populate('wishList')
+    if (!user) {
+      res.status(404).json({ ok: false, msg: "usuario no encontrado" });
+      return;
+    }
+    if (user.wishList === null || user.wishList === undefined) {
+      res.status(404).json({ ok: false, msg: "wishList vacia" });
+      return;
+    }
+    res
+      .status(200)
+      .json({ ok: true, data: user.wishList, msg: "peticion realizada con exito" });
+  } catch (error) {
+    console.error(`Error al realizar la peticion: ${error}`);
+    res
+      .status(500)
+      .json({ ok: false, msg: `Error al realizar la peticion: ${error}` });
+  }
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { AddUser, LoginUser, UpdateUser } from "../controllers/user.controller";
 import { authenticateToken } from "../helpers/token.helper";
-import { AddFavorites, DeleteFavorites } from "../controllers/userProductActions.controller";
+import { AddFavorites, DeleteFavorites, GetFavorites } from "../controllers/userProductActions.controller";
 
 const router = express.Router();
 router.use(express.json());
@@ -12,5 +12,6 @@ router.put("/users/update/:id", authenticateToken, UpdateUser);
 //user actions
 router.post('/users/add-favorites/:id', authenticateToken, AddFavorites);
 router.put('/users/delete-favorites/:id', authenticateToken, DeleteFavorites);
+router.get('/users/favorites/:id', authenticateToken, GetFavorites);
 
 export default router;
